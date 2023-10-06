f7d02166a1d3f9075b47763663c6159323c683ca
Joan Fornes <joan.fornesrayo@maibornwolff.de>
2023-10-07 00:10:08 +0200

    feat(course): added unit testing

---
+++ src/test/kotlin/com/codely/course/application/CourseCreatorTest.kt
@@ -0,0 +1,62 @@
+package com.codely.course.application
+
+import com.codely.course.BaseTest
+import com.codely.course.domain.Course
+import com.codely.course.domain.CourseRepository
+import com.codely.course.domain.InvalidCourseIdException
+import com.codely.course.domain.InvalidCourseNameException
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.time.LocalDateTime
+
+class CourseCreatorTest: BaseTest() {
+    private lateinit var courseRepository: CourseRepository
+    private lateinit var courseCreator: CourseCreator
+
+    @BeforeEach
+    fun setUp() {
+        courseRepository = mockk(relaxUnitFun = true)
+        courseCreator = CourseCreator(courseRepository)
+    }
+
+    @Test
+    fun `should create a course successfully`() {
+        givenFixedDate(fixedDate)
+        courseCreator.create(id, name)
+        thenTheCourseShouldBeSaved()
+    }
+
+    private fun thenTheCourseShouldBeSaved() {
+        verify { courseRepository.save(
+            Course.from(
+                id = id,
+                name = name,
+                createdAt = fixedDate
+            )
+        ) }
+    }
+
+    @Test
+    fun `should fail with invalid id`() {
+        givenFixedDate(fixedDate)
+
+        assertThrows<InvalidCourseIdException> { courseCreator.create("Invalid", name) }
+    }
+
+    @Test
+    fun `should fail with invalid name`() {
+        givenFixedDate(fixedDate)
+
+        assertThrows<InvalidCourseNameException> { courseCreator.create(id, "    ") }
+        assertThrows<InvalidCourseNameException> { courseCreator.create(id, "") }
+    }
+
+    companion object {
+        private const val id = "caebae03-3ee9-4aef-b041-21a400fa1bb7"
+        private const val name = "Kotlin Hexagonal Architecture Api"
+        private val fixedDate = LocalDateTime.parse("2023-10-06T00:00:00")
+    }
+}
