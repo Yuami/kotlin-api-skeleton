61614a0392ce5103a1074d06cd059a96cccadc88
Joan Fornes <joan.fornesrayo@maibornwolff.de>
2023-10-07 00:57:36 +0200

    feat(course): Create concrete implementations and add Dependency injection configuration

---
+++ src/main/kotlin/com/codely/config/DependencyInjectionConfig.kt
@@ -0,0 +1,22 @@
+package com.codely.config
+
+import com.codely.course.application.CourseCreator
+import com.codely.course.domain.Clock
+import com.codely.course.domain.CourseRepository
+import com.codely.course.infrastructure.InMemoryCourseRepository
+import com.codely.course.infrastructure.LocalDateTimeClock
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class DependencyInjectionConfig {
+
+    @Bean
+    fun courseRepository() = InMemoryCourseRepository()
+
+    @Bean
+    fun clock() = LocalDateTimeClock()
+
+    @Bean
+    fun courseCreator(courseRepository: CourseRepository, clock: Clock) = CourseCreator(courseRepository, clock)
+}
